3a02d884bbb3677670d10cf6ad9c24f30eb0d2d1
Q-Mick <quentinmick@protonmail.com>
2023-04-15 16:31:05 -0600

    moody kittens final

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let kittens = []
+loadKittens()
+
 /**
  * Called when submitting the new Kitten Form
  * This method will pull data from the form
@@ -7,6 +9,31 @@ let kittens = []
  * Then reset the form
  */
 function addKitten(event) {
+  event.preventDefault()
+  let form = event.target
+
+  if (!document.getElementById("welcome").classList.contains("hidden")){
+  getStarted()
+  }
+
+  for (let i = 0;i < kittens.length; i++){
+  if (kittens[i].name == form.name.value){
+    console.log(`That name is already taken`)
+    return
+  }
+  }
+  
+  let kitten = {
+    name: form.name.value,
+    mood: "tolerant",
+    affection: 5,
+    Id: generateId(),
+  }
+  kittens.push(kitten)
+  saveKittens()
+  form.reset()
+  toggleKittenList()
+  drawKittens()
 }
 
 /**
@@ -14,7 +41,9 @@ function addKitten(event) {
  * Saves the string to localstorage at the key kittens 
  */
 function saveKittens() {
+  window.localStorage.setItem("kittens", JSON.stringify(kittens))
 }
+
 
 /**
  * Attempts to retrieve the kittens string from localstorage
@@ -22,13 +51,37 @@ function saveKittens() {
  * the kittens array to the retrieved array
  */
 function loadKittens() {
+  let kittenData = JSON.parse(window.localStorage.getItem("kittens"))
+  if(kittenData){
+    kittens = kittenData
 }
 
-/**
+}
+
+/**    // <i class="action fa fa-trash text-danger" onclick="clearKittens('${kitten.Id}')"></i>
+ * 
+ * 
  * Draw all of the kittens to the kittens element
  */
 function drawKittens() {
-}
+ let kittenListElement = document.getElementById("kitten-list")
+  let kittenTemplate = ""
+  kittens.forEach(kitten => {
+    console.log(kitten.Id)
+    kittenTemplate +=`
+    <div class="card mt-1 mb-1">
+    <img src="${kitten.mood}-kitty.png" height="100" alt="Kitten">
+    <h3 class="mt-1 mb-1">${kitten.name}</h3>
+    <h3 class="mt-1 mb-1">Mood: ${kitten.mood}</h3>
+    <div class="d-flex justify-content-between">
+     <button onclick="pet('${kitten.Id}')">Pet kitten</button>
+     <button onclick="catnip('${kitten.Id}')">Give Catnip</button>
+     </div>
+  </div>`
+  })
+  kittenListElement.innerHTML = kittenTemplate
+
+} 
 
 
 /**
@@ -37,6 +90,12 @@ function drawKittens() {
  * @return {Kitten}
  */
 function findKittenById(id) {
+for (let i = 0; i < kittens.length; i++){
+  if (kittens[i].Id == id){
+    console.log(kittens[i].Id)
+    return i
+  }
+}
 }
 
 
@@ -49,6 +108,24 @@ function findKittenById(id) {
  * @param {string} id 
  */
 function pet(id) {
+
+  let currentKitten = findKittenById(id)
+  console.log(currentKitten)
+  let rnd = Math.round(Math.random() * 10)
+  if (rnd > 5) {
+    kittens[currentKitten].affection++;
+   } else {
+    kittens[currentKitten].affection--;
+}
+
+if (kittens[currentKitten].affection <= 0) { // if affection is 0 or lower reset to 0
+  kittens[currentKitten].affection = 0
+}
+
+if (kittens[currentKitten].affection >= 10) { // if affection is 10 or higher reset to 10
+  kittens[currentKitten].affection = 10
+}
+setKittenMood(currentKitten)
 }
 
 /**
@@ -58,6 +135,10 @@ function pet(id) {
  * @param {string} id
  */
 function catnip(id) {
+  let currentKitten = findKittenById(id)
+  console.log(currentKitten)
+  kittens[currentKitten].affection = 5
+  setKittenMood(currentKitten)
 }
 
 /**
@@ -65,6 +146,20 @@ function catnip(id) {
  * @param {Kitten} kitten 
  */
 function setKittenMood(kitten) {
+  
+console.log(kittens[kitten].affection)
+for (let i = 0;i < kittens.length; i++){
+  if (kittens[i].affection >= 7){  // if its greater than 7 set mood happy
+    kittens[i].mood = "happy"
+  }
+  else if (kittens[i].affection >= 4){ // or else check if its greater than 4 
+    kittens[i].mood = "tolerant"
+  } else {                 // if its not that either than the only other option is angry
+    kittens[i].mood = "angry"
+  }
+}
+saveKittens()
+drawKittens()
 }
 
 /**
@@ -72,6 +167,10 @@ function setKittenMood(kitten) {
  * remember to save this change
  */
 function clearKittens(){
+ kittens = []
+ console.log(kittens)
+ saveKittens()
+ toggleKittenList()
 }
 
 /**
@@ -81,8 +180,24 @@ function clearKittens(){
 function getStarted() {
   document.getElementById("welcome").remove();
   console.log('Good Luck, Take it away')
+  document.getElementById("cat-sound").play()
+  if (kittens.length == 0){
+    console.log("Kittens array is empty")
+    return
+  } else {
+    
+  toggleKittenList()
+}
 }
 
+function toggleKittenList() {
+  if (kittens.length == 0){  // if the array is empty
+    document.getElementById("kitten-list").classList.add("hidden")
+  } else if(!kittens.length == 0){
+   drawKittens()
+  document.getElementById("kitten-list").classList.remove("hidden")
+}
+}
 
 // --------------------------------------------- No Changes below this line are needed
 
@@ -101,4 +216,3 @@ function generateId() {
   return Math.floor(Math.random() * 10000000) + "-" + Math.floor(Math.random() * 10000000)
 }
 
-loadKittens();
--- readme.md
+++ readme.md
@@ -15,26 +15,26 @@ The basic design and layout for this app has been mocked out and the names of th
 ## Build Suggestions
 1. Create a form used to add a Kitten
 1. Handle the form submission
-  - Prevent the page from reloading when submitting the form
-  - Pull the form data to get a new kitten and give that kitten an id
-  - Add the new kitten to the kittens array
-  - Reset the form
-  - Save the kittens list
-  - Draw the kittens list
+  x Prevent the page from reloading when submitting the form
+  x Pull the form data to get a new kitten and give that kitten an id
+  x Add the new kitten to the kittens array
+  x Reset the form
+  x Save the kittens list
+  x Draw the kittens list
 1. Handle Page Refresh
-  - Load the kittens list
-  - Draw the kittens list
+  x Load the kittens list
+  x Draw the kittens list
 
 ## Requirements: 
-- [ ] Users must have a form where they enter in at minimum the name of a kitten 
-- [ ] A Kitten's name and mood must be visible for the user 
-- [ ] A custom Google Font must be added 
-- [ ] A Kitten's mood must affect visually the appearance of the kitten 
-- [ ] Add Kitten form clears when submitted 
-- [ ] Add Kitten form submission does not reload the page 
-- [ ] Kittens are stored in local storage 
-- [ ] Kittens can be deleted 
-- [ ] Kittens have at least two buttons that affect the kitten's mood in different ways 
-- [ ] Kitten's and their mood persist through page reloads 
-- [ ] Kitten's with the same name as an already present kitten cannot be added 
-- [ ] Kittens are visible when the page reloads
+- [x] Users must have a form where they enter in at minimum the name of a kitten 
+- [x] A Kitten's name and mood must be visible for the user 
+- [x] A custom Google Font must be added 
+- [x] A Kitten's mood must affect visually the appearance of the kitten 
+- [x] Add Kitten form clears when submitted 
+- [x] Add Kitten form submission does not reload the page 
+- [x] Kittens are stored in local storage 
+- [x] Kittens can be deleted 
+- [x] Kittens have at least two buttons that affect the kitten's mood in different ways 
+- [x] Kitten's and their mood persist through page reloads 
+- [x] Kitten's with the same name as an already present kitten cannot be added 
+- [x] Kittens are visible when the page reloads
